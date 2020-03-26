ef2e402431c4047d10424e3dc6b1a8dda7802bce
sllynn <stuart@databricks.com>
2020-03-26 11:10:47 +0000

    added project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlflow_deployments_sample_project',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLflow Deployments Sample Project',
+    author='Stuart Lynn',
+    license='',
+)
